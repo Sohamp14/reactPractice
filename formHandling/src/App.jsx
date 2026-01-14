48002610cef9b00819d07f2e94af01ad9f260804
Soham Phansekar <sohamphansekar142003@gmail.com>
2026-01-14 19:28:07 +0530

    Local storage practice

--- formHandling/src/App.jsx
+++ formHandling/src/App.jsx
@@ -5,6 +5,14 @@ const App = () => {
     e.preventDefault()
     console.log('Form submitted')
   }
+  // this is localStorage code
+  const user = { name: 'liaam', age: 40, city: 'chicago' }
+  const user1 = { name: 'sophia', age: 35, city: 'boston' }
+  console.log('User Details:', user, user1)
+  localStorage.setItem('user1', JSON.stringify(user1))
+  const usera = JSON.parse(localStorage.getItem('user1'))
+  console.log('Retrieved User:', usera)
+
 
   return (
     <div>
@@ -12,6 +20,7 @@ const App = () => {
         <input type="text" placeholder='Enter your name' />
         <button type="submit">Submit</button>
       </form>
+      APP
     </div>
   )
 }
